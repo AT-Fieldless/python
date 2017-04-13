1a644dda0597afe9b0eadf1e5ec6c09e5f8d01e4
jzy <15692118798@163.com>
2017-04-13 14:33:40 +0800

    4/13
     敏感词文本文件 filtered_words.txt，里面的内容为以下内容，当用户输入敏感词语时，则打印出 Freedom，否则打印出
    Human Rights。

---
+++ daily_exercise/0011.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# 敏感词文本文件 filtered_words.txt，里面的内容为以下内容，当用户输入敏感词语时，则打印出 Freedom，否则打印出 Human Rights。
+
+s = set()
+
+def define():
+    file = open("filtered_words.txt")
+    for eachline in file:
+        s.add(eachline.rstrip('\n'))
+
+if __name__ == '__main__':
+    define()
+    for i in s:
+        print i
+    temp = raw_input()
+    if temp in s:
+        print 'Freedom'
+    else:
+        print 'Human Rights'
